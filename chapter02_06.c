7fedd15aaa64703b7057da0ffc052eb473649db4
jiw0000 <112916241+jiw0000@users.noreply.github.com>
2023-01-05 19:41:29 +0900

    Add files via upload

---
+++ chapter02_06.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int toes = 10;
+	
+	printf(" toes = %d\n", toes );
+	printf(" toes의 제곱 = %d\n", toes * toes);
+	printf(" toes의 2배 = %d\n", toes * 2);
+
+	return 0;
+}
